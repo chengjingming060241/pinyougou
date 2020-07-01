ac4ca37d81dbb4dd248a9e33a4e96319f7478738
chengjingming <44796903+chengjingming060241@users.noreply.github.com>
2020-07-01 16:40:34 +0800

    Update SmsDemo.java

--- dysmsapi_demo_sdk__java/java/api_demo/alicom-dysms-api/src/main/java/com/alicom/dysms/api/SmsDemo.java
+++ dysmsapi_demo_sdk__java/java/api_demo/alicom-dysms-api/src/main/java/com/alicom/dysms/api/SmsDemo.java
@@ -37,8 +37,8 @@ public class SmsDemo {
     static final String domain = "dysmsapi.aliyuncs.com";
 
     // TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
-    static final String accessKeyId = "LTAIm40gsMqCN4Dz";
-    static final String accessKeySecret = "KhWSKsysIOFw4CbBSAaiviUX6YRzpn";
+    static final String accessKeyId = "";
+    static final String accessKeySecret = "";
 
     public static SendSmsResponse sendSms() throws ClientException {
 
